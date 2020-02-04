2ef2bff1db2e7c37f16dadc361e2e648b9dbfe91
Derk Norton <derk.norton@gmail.com>
2020-02-03 17:53:57 -0700

    Updated the document repository example code.

--- test-document-repository.js
+++ test-document-repository.js
@@ -10,17 +10,15 @@ const repository = require('bali-document-repository').test(notary, directory, d
 const test = async function() {
 console.log();
 
-// generate a new private notary key and public certificate
-var certificate = await notary.generateKey();
+// initialize the digital notary
+const publicKey = await notary.generateKey();
+const certificate = await notary.notarizeDocument(publicKey);
+await notary.activateKey(certificate);
 console.log('certificate: ' + certificate);
 console.log();
 
-// notarize the public certificate
-var document = await notary.notarizeDocument(certificate);
-await notary.activateKey(document);
-
 // commit the notarized public certificate to the document repository
-await repository.writeDocument(document);
+await repository.writeDocument(certificate);
 
 // create a notarized draft document
 var profile = bali.catalog({
@@ -43,24 +41,12 @@ var citation = await repository.writeDraft(draft);
 console.log('citation: ' + citation);
 console.log();
 
-// validate the citation against the draft document
-console.log('citation matches: ' + await notary.citationMatches(citation, draft));
-console.log();
-
-// verify the draft is in the repository
-console.log('draft exists: ' + await repository.draftExists(citation));
-console.log();
-
 // update the draft document
 draft = await repository.readDraft(citation);
 profile = draft.getValue('$content');
 profile.setValue('$age', 29);
 draft = await notary.notarizeDocument(profile);
 console.log('draft: ' + draft);
-console.log();
-
-// the citation is now invalid against the updated profile
-console.log('citation matches: ' + await notary.citationMatches(citation, draft));
 console.log();
 
 // save the updated draft document to the document repository
@@ -72,12 +58,8 @@ console.log('citation: ' + citation);
 console.log();
 
 // verify that the draft is no longer in the repository
-console.log('draft exists: ' + await repository.draftExists(citation));
-console.log();
-
-// read the committed document from the repository
-document = await repository.readDocument(citation);
-console.log('document: ' + document);
+const draftExists = await repository.draftExists(citation)
+console.log('draft exists: ' + draftExists);
 console.log();
 
 // name the document in the repository
