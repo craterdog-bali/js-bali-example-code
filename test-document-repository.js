22fc9f79f715f2f799f1a0b79a5da7a11500715e
Derk Norton <derk.norton@gmail.com>
2020-06-03 18:02:12 -0600

    Updated the document repository code examples.

--- test-document-repository.js
+++ test-document-repository.js
@@ -16,14 +16,11 @@ console.log();
 var certificate = await notary.generateKey();
 certificate = await notary.notarizeDocument(certificate);
 await notary.activateKey(certificate);
-console.log('certificate: ' + certificate);
-console.log();
 await storage.writeContract(certificate);
 
 // create a document
 var document = bali.instance('/acme/types/Profile/v1', {
     $name: 'Jane Doe',
-    $age: 30,
     $email: bali.reference('mailto:jane.doe@acme.org')
 });
 console.log('document: ' + document);
@@ -34,31 +31,35 @@ var citation = await repository.saveDocument(document);
 console.log('citation: ' + citation);
 console.log();
 
-// update the saved document
+// update the document
 document = await repository.retrieveDocument(citation);
 document.setValue('$age', 29);
-document.setParameter('$version', 'v1.1');
-document.setParameter('$previous', citation);
-console.log('document: ' + document);
-console.log();
-
-// save the updated document to the document repository
-citation = await repository.saveDocument(document);
-console.log('citation: ' + citation);
+console.log('updated document: ' + document);
 console.log();
 
 // commit the document to the document repository as a notarized contract
-const name = '/acme/profiles/jane/v1';
+var name = '/acme/profiles/jane/v1';
 await repository.commitDocument(name, document);
 
 // retrieve the named document from the repository
 document = await repository.retrieveDocument(name);
-console.log('document: ' + document);
+console.log('named document: ' + document);
 console.log();
 
-// retrieve the full contract from the repository
+// checkout and update a new version of the document from the repository
+const level = 2;  // version level to increment
+document = await repository.checkoutDocument(name, level);
+document.setValue('$age', 30);
+console.log('next version: ' + document);
+console.log();
+
+// commit the new version to the document repository
+name = '/acme/profiles/jane/v1.1';
+await repository.commitDocument(name, document);
+
+// retrieve the notarized contract for the new version from the repository
 const contract = await repository.retrieveContract(name);
-console.log('contract: ' + contract);
+console.log('notarized contract: ' + contract);
 console.log();
 };
 
